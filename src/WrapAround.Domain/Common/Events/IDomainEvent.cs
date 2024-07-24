4da84ac272df25fbb5e422879ecbf4cfca36b70b
Chris Howarth <chowarth@users.noreply.github.com>
2024-07-24 15:52:06 +0100

    Add domain base classes

---
+++ src/WrapAround.Domain/Common/Events/IDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WrapAround.Domain.Common.Events;
+
+/// <summary>
+/// Marker interface for domain events.
+/// </summary>
+public interface IDomainEvent
+{
+}
---
+++ src/WrapAround.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,26 @@
+﻿using WrapAround.Domain.Common.Events;
+
+namespace WrapAround.Domain.Common.Models;
+
+public abstract class AggregateRoot : Entity
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents
+        => _domainEvents.AsReadOnly();
+
+    protected AggregateRoot(Guid id)
+        : base(id)
+    {
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
