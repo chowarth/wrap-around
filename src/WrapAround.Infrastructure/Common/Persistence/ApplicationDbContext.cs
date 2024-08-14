8982176195204189a5e715acfa1cc9054aba6937
Chris Howarth <chowarth@users.noreply.github.com>
2024-08-14 13:33:05 +0100

    Remove sessions DbSet

--- src/WrapAround.Infrastructure/Common/Persistence/ApplicationDbContext.cs
+++ src/WrapAround.Infrastructure/Common/Persistence/ApplicationDbContext.cs
@@ -8,8 +8,6 @@ namespace WrapAround.Infrastructure.Common.Persistence;
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<User>(options)
 {
-    public DbSet<Session> Sessions { get; init; }
-
     public DbSet<Student> Students { get; init; }
 
     protected override void OnModelCreating(ModelBuilder builder)
