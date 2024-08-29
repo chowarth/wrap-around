9049a7349dfd55e97a0abe4b3dc8f09ffc14f3b5
Chris Howarth <chowarth@users.noreply.github.com>
2024-08-29 15:11:48 +0100

    Remove comment

--- src/WrapAround.Migrations/Worker.cs
+++ src/WrapAround.Migrations/Worker.cs
@@ -32,15 +32,6 @@ public class Worker : BackgroundService
             using var scope = _serviceProvider.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-            // Why are a multiple (4) connection exceptions thrown before the migrations then run and succeed?
-            // Microsoft.EntityFrameworkCore.Infrastructure[10404]
-            // A transient exception occurred during execution. The operation will be retried after 7549ms.
-            //     Microsoft.Data.SqlClient.SqlException (0x80131904): A connection was successfully established with the server, but then an error occurred during the pre-login handshake. (provider: TCP Provider, error: 0 - An existing connection was forcibly closed by the remote host.)
-            //     ---> System.ComponentModel.Win32Exception (10054): An existing connection was forcibly closed by the remote host.
-
-            // Is this because it's trying to run before the database has started?
-            // Add the WaitFor dependency in AppHost
-
             await dbContext.Database.MigrateAsync(stoppingToken);
         }
         catch (Exception ex)
